d2f600b8971f32bbedbc87c110dca6a3fe2d8a3f
satrajit <satrajit.roy@expeto.io>
2024-02-15 08:30:43 -0500

    Optimized some code Fixed integer partition

--- backtrack.py
+++ backtrack.py
@@ -1,11 +1,4 @@
-import sys
-
-def backtrack(n, k, j, i, reject, domain, count):
-  l = 1
-  if l <= n:
-    test(k, l, 0, n, 1, j, i, [0 for i in range(n + 1)], reject, domain, count)
-
-def test(k, l, m, n, t, j, i, x, reject, domain, count):
+def test(n, k, j, i, l, m, t, x, reject, domain, count):
   while True:
     if reject(l, n, t, x):
       if domain(t, n, k):
@@ -13,44 +6,49 @@ def test(k, l, m, n, t, j, i, x, reject, domain, count):
       else:
         m, l, t = revert(k, l - 1, m, n, x, domain)
     else:
-      l = set_state(l, t, x)
+      x[l] = t
+      l = l + 1
       if count(l, n, k):
         t = i
       else:
         print("Solution: " + str(m) + " -> " + str(x[1:j + 1]) + "\n")
         m, l, t = revert(k, l - 1, m + 1, n, x, domain)
+    if l < 1:
+      break
 
-def set_state(l, t, x):
-  x[l] = t
-  return l + 1
 
 def revert(k, l, m, n, x, domain):
-  if l <= 0:
-    sys.exit(0)
+  if l < 1:
+    return m, l, x[0]
 
-  t = reset_state(l, n, x)
+  t = x[l]
   if domain(t, n, k):
     t = t + 1
     return m, l, t
 
-  return revert(k, l - 1, m, l, n, x)
+  return revert(k, l - 1, m, n, x, domain)
 
-def reset_state(l, n, x):
-  return x[l]
 
-if __name__ == "__main__":
-  # backtrack(12, 6, 6, 1, lambda l, n, t, x: False, lambda t, n, k: t < n, lambda t, n, k: t <= k)  # n-tuple
-  # backtrack(12, 6, 6, 1, lambda l, n, t, x: any(s == t for s in x[1:l]), lambda t, n, k: t < n,
-  #           lambda t, n, k: t <= k)  ##permute
-  # backtrack(12, 6, 6, 1, lambda l, n, t, x: any(s <= t for s in x[1:l]), lambda t, n, k: t < n,
-  #           lambda t, n, k: t <= k)  ##combine
-  # backtrack(12, 12, 12, 1, lambda l, n, t, x: t > 1 + max(x[0:l]), lambda t, n, k: bool(t < k),
-  #           lambda l, n, k: bool(l <= n))  # partition
-  backtrack(12, 1, 12, 0,
-            lambda l, n, t, x: (l > 1 and t > x[l - 1]) or \
-                               ((t + sum(x[i] for i in range(1, l))) > n) or \
-                               (n - (n - l) * t) > (t + sum(x[i] for i in range(1, l))) or \
-                               False,
-            lambda t, n, k: t < 1 + n // k,
-            lambda l, n, k: l <= n)  # integer partition
-
+# if __name__ == "__main__":
+  # test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: False, lambda t, n, k: t < n,
+  #      lambda t, n, k: t <= k)  # n-tuple
+  # test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: any(s == t for s in x[1:l]),
+  #      lambda t, n, k: t < n,
+  #      lambda t, n, k: t <= k)  ##permute
+  # test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: any(s <= t for s in x[1:l]),
+  #      lambda t, n, k: t < n,
+  #      lambda t, n, k: t <= k)  ##combine
+  # test(12, 12, 12, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: t > 1 + max(x[0:l]),
+  #      lambda t, n, k: bool(t < k),
+  #      lambda l, n, k: l <= n)  # partition
+  # test(20, 1, 20, 0, 1, 0, 1, [0] * 21,
+  #      lambda l, n, t, x: (l > 1 and t > x[l - 1]) or \
+  #                         ((t + sum(x[i] for i in range(1, l))) > n) or \
+  #                         (n - (n - l) * t) > (t + sum(x[i] for i in range(1, l))) or \
+  #                         False,
+  #      lambda t, n, k: t < 1 + n // k,
+  #      lambda l, n, k: l <= n)  # integer partition
+  # test(12, 12, 12, 1, 1, 0, 1, [0] * 21,
+  #      lambda l, n, t, x: any(s == t or abs(s - t) == abs(1 + i - l) for i, s in enumerate(x[1:l])),
+  #      lambda t, n, k: bool(t < n),
+  #      lambda l, n, k: l <= n)  # n-queens
