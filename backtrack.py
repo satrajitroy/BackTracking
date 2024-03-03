be270b49d6ec07df5beb964314342f0f90d51443
WINDOWS-1A9SGJ6\Satrajit <satrajit.roy@gmail.com>
2024-03-03 17:58:34 -0500

    Exact cover using dancing links a la Knuth

--- backtrack.py
+++ backtrack.py
@@ -1,4 +1,5 @@
 def test(n, k, j, i, l, m, t, x, reject, domain, count):
+  y = []
   while True:
     if reject(l, n, t, x):
       if domain(t, n, k):
@@ -11,10 +12,13 @@ def test(n, k, j, i, l, m, t, x, reject, domain, count):
       if count(l, n, k):
         t = i
       else:
+        y.append(x[1:j + 1])
         print("Solution: " + str(m) + " -> " + str(x[1:j + 1]) + "\n")
         m, l, t = revert(k, l - 1, m + 1, n, x, domain)
     if l < 1:
       break
+
+  return y
 
 
 def revert(k, l, m, n, x, domain):
@@ -29,26 +33,26 @@ def revert(k, l, m, n, x, domain):
   return revert(k, l - 1, m, n, x, domain)
 
 
-if __name__ == "__main__":
-  test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: False, lambda t, n, k: t < n,
-       lambda t, n, k: t <= k)  # n-tuple
-  test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: any(s == t for s in x[1:l]),
-       lambda t, n, k: t < n,
-       lambda t, n, k: t <= k)  ##permute
-  test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: any(s <= t for s in x[1:l]),
-       lambda t, n, k: t < n,
-       lambda t, n, k: t <= k)  ##combine
-  test(12, 12, 12, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: t > 1 + max(x[0:l]),
-       lambda t, n, k: bool(t < k),
-       lambda l, n, k: l <= n)  # partition
-  test(20, 1, 20, 0, 1, 0, 1, [0] * 21,
-       lambda l, n, t, x: (l > 1 and t > x[l - 1]) or
-                          ((t + sum(x[i] for i in range(1, l))) > n) or
-                          (n - (n - l) * t) > (t + sum(x[i] for i in range(1, l))) or
-                          False,
-       lambda t, n, k: t < 1 + n // k,
-       lambda l, n, k: l <= n)  # integer partition
-  test(20, 20, 20, 1, 1, 0, 1, [0] * 21,
-       lambda l, n, t, x: any(s == t or abs(s - t) == abs(1 + i - l) for i, s in enumerate(x[1:l])),
-       lambda t, n, k: bool(t < n),
-       lambda l, n, k: l <= n)  # n-queens
+# if __name__ == "__main__":
+#   test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: False, lambda t, n, k: t < n,
+#        lambda t, n, k: t <= k)  # n-tuple
+#   test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: any(s == t for s in x[1:l]),
+#        lambda t, n, k: t < n,
+#        lambda t, n, k: t <= k)  ##permute
+#   test(12, 6, 6, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: any(s <= t for s in x[1:l]),
+#        lambda t, n, k: t < n,
+#        lambda t, n, k: t <= k)  ##combine
+#   test(12, 12, 12, 1, 1, 0, 1, [0] * 21, lambda l, n, t, x: t > 1 + max(x[0:l]),
+#        lambda t, n, k: bool(t < k),
+#        lambda l, n, k: l <= n)  # partition
+#   test(20, 1, 20, 0, 1, 0, 1, [0] * 21,
+#        lambda l, n, t, x: (l > 1 and t > x[l - 1]) or
+#                           ((t + sum(x[i] for i in range(1, l))) > n) or
+#                           (n - (n - l) * t) > (t + sum(x[i] for i in range(1, l))) or
+#                           False,
+#        lambda t, n, k: t < 1 + n // k,
+#        lambda l, n, k: l <= n)  # integer partition
+#   test(20, 20, 20, 1, 1, 0, 1, [0] * 21,
+#        lambda l, n, t, x: any(s == t or abs(s - t) == abs(1 + i - l) for i, s in enumerate(x[1:l])),
+#        lambda t, n, k: bool(t < n),
+#        lambda l, n, k: l <= n)  # n-queens
--- xcc_dlx.py
+++ xcc_dlx.py
@@ -1,8 +1,10 @@
 import io
+import itertools
 import string
 import sys
 from random import choices, randint, sample
 from time import time_ns
+from backtrack import test
 
 from psutil import virtual_memory
 
@@ -67,19 +69,21 @@ def mrv():
   print(f"{mrv.__name__}\nlength: {length}\nleft. : {left}\nright : {right}")
   theta = sys.maxsize
   p = right[0]
+  i = p
   while p != 0:
     print(f"{mrv.__name__} p: {p}")
     lamda = length[p]
     if lamda < theta:
       theta = lamda
+      i = p
       if theta == 0:
-        print(f"{mrv.__name__} i: {p}")
+        print(f"{mrv.__name__} i: {i}")
         break
 
     p = right[p]
 
-  print(f"{mrv.__name__} p: {p}")
-  return p
+  print(f"{mrv.__name__} i: {i}")
+  return i
 
 
 # step 4:
@@ -137,13 +141,13 @@ def try_l(i):
 
 
 def retry_l():
-  print(retry_l.__name__)  
+  print(retry_l.__name__)
   p = x[l] - 1
   while p != x[l]:
     j = top[p]
     if j > 0:
       uncover(j)
-      p -+ 1
+      p -= 1
     else:
       p = down[p]
 
@@ -339,7 +343,7 @@ def setup(bytes):
   bytes.seek(0)
   s = bytes.readline().decode('utf-8').strip()  # read line
   o = s.split(',')
-  m = int(o[0]) + 2
+  m = int(o[0]) + 1
   l = int(o[2])
   L = int(o[3]) + 1
 
@@ -387,7 +391,7 @@ def setup(bytes):
 
     o = s.split('.')
     k = int(o[0])
-    t = [int(a) for a in o[1][1:-1].split(',')]
+    t = [int(a) for a in filter(None, o[1][1:-1].strip().split(','))]
     for j in range(1, k + 1):
       K = t[j - 1]
       length[K] += 1
@@ -415,7 +419,7 @@ def setup(bytes):
   return (N, M, p)
 
 
-def generate(N):
+def randomized(N):
   mem = virtual_memory().available
   n = -1
   i = 0
@@ -451,12 +455,66 @@ def generate(N):
   print("Memory used after writing options: " + "{:,}".format(mem - virtual_memory().available))
   return bytes
 
+def specified(N, items):
+  mem = virtual_memory().available
+  n = -1
+  i = 0
+
+  bytes = io.BytesIO()
+  k = randint(4, 8)
+  prefix = ''.join(choices(string.ascii_letters + string.digits, k=k))
+
+  n_val = N
+  o_val = len(items)
+  m_vals = [tuple(sorted(items[i])) for i in range(o_val)]
+
+  print("Memory used: after generating options " + "{:,}".format(mem - virtual_memory().available))
+
+  m_vals = set(tuple(itertools.chain(*m_vals)))
+  o = [str(n_val), prefix, str(len(m_vals)), str(2 + sum(l + 2 for l in [len(m_val) for m_val in m_vals]))]
+  bytes.write(','.join(o).encode() + b'\n')  # write string encoded as bytes
+
+  print("Memory used after unique options: " + "{:,}".format(mem - virtual_memory().available))
+
+  for m_val in m_vals:
+    o = [str(len(m_val))]
+    o.extend([str(m_val)])
+    bytes.write('.'.join(o).encode() + b'\n')
+
+  print("Serialized size: " + "{:,}".format((bytes.seek(0, io.SEEK_END))))
+  bytes.seek(0)  # reset the stream position
+
+  for line in bytes:
+    print(line.decode())  # decode bytes to string
+
+  print("Memory used after writing options: " + "{:,}".format(mem - virtual_memory().available))
+  return bytes
+
+def convert_rgs(s):
+  p = []
+  for i, v in enumerate(s):
+    v = int(v)
+    if v == 0:
+      continue
+    if v > len(p):
+      p.append({i+1})
+    else:
+      p[v - 1].add(i+1)
+  return [tuple(q) for q in p]
+
 
 if __name__ == "__main__":
   global N, M, Z, l, x
 
   now = time_ns()
-  bytes = generate((6))
+  N = 7
+  x = test(N, N, N, 1, 1, 0, 1, [0] * (N+1),
+       lambda l, n, t, x: t > 1 + max(x[0:l]),
+       lambda t, n, k: bool(t < k),
+       lambda l, n, k: l <= n)  # partition
+
+  items = [tuple(convert_rgs(y)) for y in sample(x, N)]
+  bytes = specified(N, items) # specified(4, items)
   print("Generate: "+"{:,}".format(int((time_ns()-now)//1e6)))
   now = time_ns()
   (N, M, Z) = setup(bytes)
